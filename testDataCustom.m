abfd7835c3c52bd937c1296a4fcd11f514fdc102
iOS-Blog <info@markpetherbridge.co.uk>
2014-11-22 04:55:30 +0000

    Initial Commit

---
+++ testDataCustom.m
@@ -0,0 +1,9 @@
+//
+//  testDataCustom.m
+//  testData
+//
+//  Created by  -  on 22/11/2014.
+//  Copyright (c) 2014 iOS-Blog. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ testDataTests/testDataTests.swift
@@ -0,0 +1,35 @@
+//
+//  testDataTests.swift
+//  testDataTests
+//
+//  Created by  -  on 22/11/2014.
+//  Copyright (c) 2014 iOS-Blog. All rights reserved.
+//
+
+import XCTest
+
+class testDataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
